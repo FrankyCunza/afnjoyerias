32bd08afd53c51a2d83eedda4836ca9c99435bb4
Franky Cunza <franky.cunza@prix.tips>
2020-02-29 00:09:02 -0500

    theme wordpress

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+<div class="container">
+        <div class="row">
+            <div class="logo">
+                <img src="assets/images/logo.svg" alt="" class="logo__src">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-2">
+                <div class="card homeProducts__card">
+                    <img src="assets/images/one.png" alt="" class="homeProducts__img">
+                </div>
+            </div>
+            <div class="col col-md-2">
+                <div class="card homeProducts__card">
+                    <img src="assets/images/two.png" alt="" class="homeProducts__img">
+                </div>
+            </div>
+            <div class="col col-md-2">
+                <div class="card homeProducts__card">
+                    <img src="assets/images/one.png" alt="" class="homeProducts__img">
+                </div>
+            </div>
+            <div class="col col-md-2">
+                <div class="card homeProducts__card">
+                    <img src="assets/images/one.png" alt="" class="homeProducts__img">
+                </div>
+            </div>
+            <div class="col col-md-2">
+                <div class="card homeProducts__card">
+                    <img src="assets/images/one.png" alt="" class="homeProducts__img">
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
