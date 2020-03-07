e9e9dbe8145726de8a2cf2d8493c6f00602387dc
Franky Cunza <franky.cunza@prix.tips>
2020-03-06 22:08:23 -0500

    admin bar

--- functions.php
+++ functions.php
@@ -2,5 +2,22 @@
     function esilos_y_js(){
         wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0');
     }
-    add_action('wp_enqueue_scripts', 'esilos_y_js');
+	add_action('wp_enqueue_scripts', 'esilos_y_js');
+	
+
+	//Ocultar barra de herramientas para los usuarios 
+	add_action('after_setup_theme', 'remove_admin_bar');
+
+	function remove_admin_bar() {
+
+		if (!current_user_can('administrator') && !is_admin()) {
+
+			show_admin_bar(false);
+
+		}
+
+	}
+
+
+
 ?>
