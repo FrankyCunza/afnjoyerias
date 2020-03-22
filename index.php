7e0487e338c36a2471912bbaaa6089996adb6dda
Franky Cunza <franky.cunza@prix.tips>
2020-03-22 15:11:44 -0500

    add styles for responsive

--- index.php
+++ index.php
@@ -76,29 +76,29 @@
             <div class="col col-md-2">
                 <div class="card homeProducts__card">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/earrings/1.png" alt="" class="homeProducts__img">
-                    <h2 class="homeProducts__title">Anillo de Plata 950</h2>
-                    <p class="homeProducts__text">con piedra de color citrino</p>
+                    <h2 class="homeProducts__title">Aretees Rositas</h2>
+                    <!-- <p class="homeProducts__text">con piedra de color citrino</p> -->
                 </div>
             </div>
             <div class="col col-md-2">
                 <div class="card homeProducts__card">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/earrings/2.png" alt="" class="homeProducts__img">
-                    <h2 class="homeProducts__title">Anillo de Plata 950</h2>
-                    <p class="homeProducts__text">con piedra de color lila</p>
+                    <h2 class="homeProducts__title">Aretes Gatuno</h2>
+                    <!-- <p class="homeProducts__text">con piedra de color lila</p> -->
                 </div>
             </div>
             <div class="col col-md-2">
                 <div class="card homeProducts__card">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/earrings/3.png" alt="" class="homeProducts__img">
-                    <h2 class="homeProducts__title">Anillo de Plata 950</h2>
-                    <p class="homeProducts__text">con dos piedritas de zircon</p>
+                    <h2 class="homeProducts__title">Aretes Delfín</h2>
+                    <!-- <p class="homeProducts__text">con dos piedritas de zircon</p> -->
                 </div>
             </div>
             <div class="col col-md-2">
                 <div class="card homeProducts__card">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/earrings/4.png" alt="" class="homeProducts__img">
-                    <h2 class="homeProducts__title">Aros de Matrimonio</h2>
-                    <p class="homeProducts__text">con detalles de piedras de zircon</p>
+                    <h2 class="homeProducts__title">Aretes Estrellas</h2>
+                    <!-- <p class="homeProducts__text">con detalles de piedras de zircon</p> -->
                 </div>
             </div>
         </div>
