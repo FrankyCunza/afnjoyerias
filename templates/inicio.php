5c1a4664cf2f9c8343adee3290d80eb48e6faa08
Franky Cunza <franky.cunza@prix.tips>
2020-05-15 17:50:50 -0500

    backup

--- templates/inicio.php
+++ templates/inicio.php
@@ -21,8 +21,30 @@
 	  )
     );
 ?>
-
 <div class="container">
+    <div class="row">
+        <div class="col col-md-12">
+            <p class="home__text"><?php the_field( 'text' ); ?></p>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+         <div class="owl-carousel owl-theme">
+            <div class="item"><h4>1</h4></div>
+            <div class="item"><h4>2</h4></div>
+            <div class="item"><h4>3</h4></div>
+            <div class="item"><h4>4</h4></div>
+            <div class="item"><h4>5</h4></div>
+            <div class="item"><h4>6</h4></div>
+            <div class="item"><h4>7</h4></div>
+            <div class="item"><h4>8</h4></div>
+            <div class="item"><h4>9</h4></div>
+            <div class="item"><h4>10</h4></div>
+            <div class="item"><h4>11</h4></div>
+            <div class="item"><h4>12</h4></div>
+        </div>
+    </div>
     
     <div class="row">
         <h2 class="title__two">Anillos</h2>
