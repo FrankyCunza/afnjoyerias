d6311ce710d1fcd11257503e5f66d8b468bd1b8d
Franky Cunza <franky.cunza@prix.tips>
2020-05-16 23:38:08 -0500

    finis testimonios

--- parts/testimonies.php
+++ parts/testimonies.php
@@ -8,7 +8,7 @@
         <div class="homeOpinions__boxes">
             <div class="homeOpinions__box">
                 <div class="homeOpinions__image">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/opinion1.png" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/testimonio1.png" alt="">
                 </div>
                 <div class="homeOpinions__stars">
                     <ul class="homeOpinions__stars__menu">
@@ -20,9 +20,13 @@
                     </ul>
                 </div>
                 <div class="homeOpinions__stars"></div>
-                <p class="homeOpinions__name">Juana Martinez</p>
+                <p class="homeOpinions__name">Brigitte Yanina Deza Delgado</p>
                 <div class="homeOpinions__country">España</div>
-                <p class="homeOpinions__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                <p class="homeOpinions__text">
+                Las joyas que recibí de samis cada una fue tal cual lo imaginé. 
+                Cada detalle que tiene cada una de ellas son realmente hermosos, su calidad y servicio te permiten estar tan satisfechos con lo recibido.
+                Si estás soñando con alguna joya no lo dudes más, tienes a la mejor.
+                </p>
             </div>
         </div>
     </div>
@@ -30,7 +34,7 @@
         <div class="homeOpinions__boxes">
             <div class="homeOpinions__box">
                 <div class="homeOpinions__image">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/opinion2.png" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/testimonio2.png" alt="">
                 </div>
                 <div class="homeOpinions__stars">
                     <ul class="homeOpinions__stars__menu">
@@ -41,9 +45,13 @@
                         <li class="homeOpinions__stars__list"><i class="fas fa-star"></i></li>
                     </ul>
                 </div>
-                <p class="homeOpinions__name">Juana Martinez</p>
+                <p class="homeOpinions__name">Rosario Karina López Cadillo</p>
                 <div class="homeOpinions__country">España</div>
-                <p class="homeOpinions__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                <p class="homeOpinions__text">
+                Feliz por las joyas que recibí de AFN, me encantaron.
+                Todo fue como lo esperaba, el servicio y la calidad de primera.
+                Los mejores sin dudarlo.
+                </p>
             </div>
         </div>
     </div>
@@ -51,7 +59,7 @@
         <div class="homeOpinions__boxes">
             <div class="homeOpinions__box">
                 <div class="homeOpinions__image">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/opinion3.png" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/testimonio3.png" alt="">
                 </div>
                 <div class="homeOpinions__stars">
                     <ul class="homeOpinions__stars__menu">
@@ -63,9 +71,14 @@
                     </ul>
                 </div>
                 <div class="homeOpinions__stars"></div>
-                <p class="homeOpinions__name">Juana Martinez</p>
+                <p class="homeOpinions__name">Orfelina Poma Castro</p>
                 <div class="homeOpinions__country">España</div>
-                <p class="homeOpinions__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                <p class="homeOpinions__text">
+                Toda reina merece tener corona!  Joyerías AFN es una firma de joyas personalizadas para mujeres,
+                piezas de joyería hechas en oro, plata, plata bañada y otros materiales de alta calidad. Con una extensa gama de propuestas
+                para matrimonios y toda ocasión, en sus colecciones también se presentan bijouterie y accesorios., les recomiendo a ojos cerrados,
+                mi favorita Joyerías AFN.
+                </p>
             </div>
         </div>
     </div>
